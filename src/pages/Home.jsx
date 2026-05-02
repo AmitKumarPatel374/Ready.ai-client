3856f8e1f24d5a184f13ffb4a5224159a3373138
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2026-05-02 11:25:00 +0530

    subcription plan component added on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
+import Plan from '../components/Plan'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <AiTools />
+      <Plan />
     </div>
   )
 }
