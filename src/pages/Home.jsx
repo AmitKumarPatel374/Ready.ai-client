69516a36626ab1749e2f14e5ce4174cf60023e50
Amit Kumar Patel <amitpatel9302352967@gmail.com>
2026-05-03 11:02:00 +0530

    footer component in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
 import Plan from '../components/Plan'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Hero />
       <AiTools />
       <Plan />
+      <Footer />
     </div>
   )
 }
